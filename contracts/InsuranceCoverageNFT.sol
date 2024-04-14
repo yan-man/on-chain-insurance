57f617524d604c00f60e829212d8dee5408af925
y-b-m <yanbinman@gmail.com>
2024-04-13 19:41:49 -0500

    add tests for supportsInterface;

--- contracts/InsuranceCoverageNFT.sol
+++ contracts/InsuranceCoverageNFT.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.24;
 
 import {AccessControlEnumerable} from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract InsuranceCoverageNFT is AccessControlEnumerable, ERC721 {
+contract InsuranceCoverageNFT is AccessControlEnumerable, ERC721Enumerable {
     bytes32 public constant MANAGER_CONTRACT = keccak256("MANAGER_CONTRACT");
 
     constructor(address managerContract_) ERC721("InsuranceCoverage", "ICNFT") {
@@ -16,9 +16,23 @@ contract InsuranceCoverageNFT is AccessControlEnumerable, ERC721 {
     // mint - to mint NFT, manager contract only
     // burn - to burn NFT, owner only
     //
+
+    // function mint(address to, uint256 tokenId) external {
+    //     require(
+    //         hasRole(MANAGER_CONTRACT, msg.sender),
+    //         "InsuranceCoverageNFT: must have manager role to mint"
+    //     );
+    //     _safeMint(to, tokenId);
+    // }
+
     function supportsInterface(
         bytes4 interfaceId
-    ) public view override(ERC721, AccessControlEnumerable) returns (bool) {
+    )
+        public
+        view
+        override(ERC721Enumerable, AccessControlEnumerable)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 }
--- test/foundry/InsuranceCoverageNFT.t.sol
+++ test/foundry/InsuranceCoverageNFT.t.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 import {CustomTest} from "../helpers/CustomTest.t.sol";
 import {Test, console} from "forge-std/Test.sol";
@@ -24,5 +26,33 @@ contract InsuranceCoverageNFTTest is Test, CustomTest {
         deployInsuranceCoverageNFT.setConstructorArgs(args);
 
         insuranceCoverageNFT = deployInsuranceCoverageNFT.run();
+    }
+
+    function test_supportsInterface_success() public view {
+        // ERC-721 interface ID
+        bytes4 _erc721InterfaceID = type(IERC721).interfaceId;
+        assertTrue(insuranceCoverageNFT.supportsInterface(_erc721InterfaceID));
+
+        // ERC-721 Enumerable interface ID
+        bytes4 _erc721EnumerableInterfaceID = type(IERC721Enumerable)
+            .interfaceId;
+        assertTrue(
+            insuranceCoverageNFT.supportsInterface(_erc721EnumerableInterfaceID)
+        );
+
+        // Access Control interface ID
+        bytes4 _accessControlInterfaceID = type(IAccessControl).interfaceId;
+        assertTrue(
+            insuranceCoverageNFT.supportsInterface(_accessControlInterfaceID)
+        );
+    }
+
+    function test_supportsInterface_fail(bytes4 interfaceId) public view {
+        vm.assume(
+            interfaceId != type(IERC721).interfaceId &&
+                interfaceId != type(IERC721Enumerable).interfaceId &&
+                interfaceId != type(IAccessControl).interfaceId
+        );
+        assertFalse(insuranceCoverageNFT.supportsInterface(interfaceId));
     }
 }
